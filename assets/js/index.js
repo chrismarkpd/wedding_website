a586cc803dedcbd5c973e3dc406866836c18dd79
chrismarkpd <chdmark@gmail.com>
2020-03-14 21:10:08 -0700

    Function for animation

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  triggerAnimation();
+});
+
+function triggerAnimation() {
+  var moreInfo = $('.more-info');
+  $.each(moreInfo, function( i, val ) {
+    setTimeout(function() {
+      $(val).css('display', 'block');
+    }, 1500 * i);
+  })
+}
